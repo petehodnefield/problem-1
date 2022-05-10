560314dabda0d73a9a6109f9388086cc88e2ed68
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-05-10 09:34:00 -0500

    Initial commit

--- myApp.js
+++ myApp.js
@@ -1,8 +1,13 @@
 let express = require('express');
 let app = express();
 
+app.get('/', (req, res) => {
+    console.log("Hello World")
+})
 
-
+app.listen(3001, () => {
+    console.log("Hello World")
+})
 
 
 
